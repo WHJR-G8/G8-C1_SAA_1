1cf09c9f380cdb911060690d5c46d092f2b7ad43
WHJR-G8 <84369631+WHJR-G8@users.noreply.github.com>
2021-10-12 15:15:42 +0530

    Update Sol_SAA_1.py

--- Sol_SAA_1.py
+++ Sol_SAA_1.py
@@ -1,12 +1,19 @@
 import turtle
+
 turtle.color("red")
+
 turtle.pensize(10)
+
 side = 100
+
 turtle.forward(side)
 turtle.left(90)
+
 turtle.forward(side)
 turtle.left(90)
+
 turtle.forward(side)
 turtle.left(90)
+
 turtle.forward(side)
 turtle.left(90)
